79e7fede20066982372d97c3361f1189a513f77d
Uma2708 <bhardwajuma001@gmail.com>
2024-05-31 20:55:21 +0530

    commit

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Overview.css';
+import { FaMoneyBillAlt, FaHashtag, FaUserCheck, FaBox } from 'react-icons/fa';
 
 const Overview = () => {
   return (
@@ -9,7 +10,7 @@ const Overview = () => {
         <div className="card">
           <div className='card_name'>
             <div>Hoken Balance</div>
-            <div>icon</div>
+            <div><FaMoneyBillAlt /></div>
           </div>
           <div className='card_money'>$56,674</div>
           <div className='card_bottum'>
@@ -22,42 +23,40 @@ const Overview = () => {
 
         <div className="card">
           <div className='card_name'>
-            <div>Hoken Balance</div>
-            <div>icon</div>
+            <div>Hash Unit</div>
+            <div><FaHashtag /></div>
           </div>
           <div className='card_money'>$56,674</div>
           <div className='card_bottum'>
             <div className='cord_bottum_title'>
-              <div>Upcoming Balance : 200</div>
-              <div>Available Balance : 100</div>
+              <div>Deposit</div>
             </div>
           </div>
         </div>
 
         <div className="card">
           <div className='card_name'>
-            <div>Hoken Balance</div>
-            <div>icon</div>
+            <div>Current Rank</div>
+            <div><FaUserCheck/></div>
           </div>
           <div className='card_money'>$56,674</div>
           <div className='card_bottum'>
             <div className='cord_bottum_title'>
-              <div>Upcoming Balance : 200</div>
-              <div>Available Balance : 100</div>
+              <div>Initiate Gainer</div>
             </div>
           </div>
         </div>
         
         <div className="card">
           <div className='card_name'>
-            <div>Hoken Balance</div>
-            <div>icon</div>
+            <div>Self Package</div>
+            <div><FaBox/></div>
           </div>
           <div className='card_money'>$56,674</div>
           <div className='card_bottum'>
             <div className='cord_bottum_title'>
-              <div>Upcoming Balance : 200</div>
-              <div>Available Balance : 100</div>
+              <div>Stater</div>
+              
             </div>
           </div>
         </div>
